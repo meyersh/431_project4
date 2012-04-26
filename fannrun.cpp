024737f0c3efac88db519aa94115d75bf68aad6c
Shaun Meyer <meyersh@morningside.edu>
2012-04-25 19:11:21 -0500

    Modified fannrun and fanntest to model the identity function: cool beans.

--- fannrun.cpp
+++ fannrun.cpp
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include "floatfann.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+template <class V> string printArray(V *array, int len) {
+    stringstream ss;
+    ss << "[";
+    for (int i = 0; i < len; i++) {
+        ss << array[i];
+        if (i < len-1)
+            ss << ", ";
+    }
+
+    ss << "]";
+
+    return ss.str();
+}
 
 int main()
 {
     fann_type *calc_out;
-    fann_type input[2];
+    fann_type input[8];
 
-    struct fann *ann = fann_create_from_file("xor_float.net");
+    struct fann *ann = fann_create_from_file("identity_float.net");
 
-    input[0] = -1;
+    input[0] = 0;
     input[1] = 1;
+    input[2] = 0;
+    input[3] = 0;
+    input[4] = 0;
+    input[5] = 0;
+    input[6] = 0;
+    input[7] = 0;
+
     calc_out = fann_run(ann, input);
 
-    printf("xor test (%f,%f) -> %f\n", input[0], input[1], calc_out[0]);
+    cout << "Identity test:" << endl
+         << printArray(input, 8) << endl
+         << printArray(calc_out, 8) << endl;
+    
 
     fann_destroy(ann);
     return 0;
