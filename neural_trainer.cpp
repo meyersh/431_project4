bac80521196fce540eb57a3124aabbecd8b0eea2
Shaun Meyer <meyersh@morningside.edu>
2012-04-26 15:59:54 -0500

    Modified neural_trainer slightly.   - setenv DEBUG=yes for game output in p->playToken(r,c, COLOR) form for     easy pasting into neural.cpp   - setenv VERBOSE=yes for very noisy output.

--- neural_trainer.cpp
+++ neural_trainer.cpp
@@ -25,12 +25,12 @@ int main(int argc, char **argv) {
 
     // for init, don't read in the file but init to 0's.
     if (argc > 1 && string(argv[1]) == "init") {
-        const unsigned int num_layers = 4;
+        const unsigned int num_layers = 3;
 
-        const unsigned int num_input = p1->Board.size()+2; // plus ours and theirs captures.
+        const unsigned int num_input = 20; // plus ours and theirs captures.
         const unsigned int num_output = 1;
         
-        const unsigned int num_neurons_hidden_a = p1->Board.size()*2;
+        const unsigned int num_neurons_hidden_a = 45;
         const unsigned int num_neurons_hidden_b = 35;
 
         const float desired_error = (const float) 0.001;
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
         struct fann *ann = fann_create_standard(num_layers,
                                                 num_input,
                                                 num_neurons_hidden_a,
-                                                num_neurons_hidden_b,
+                                                //num_neurons_hidden_b,
                                                 num_output);
 
         fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
@@ -52,6 +52,8 @@ int main(int argc, char **argv) {
     }
 
     weights.load();
+    cout << "Weights: " <<
+        weights.toString() << endl;
 
 	// Play a game with 0 players.
 	while (p1->gameOutcome(WHITE) == 0) {
@@ -71,7 +73,11 @@ int main(int argc, char **argv) {
 		p2->playToken(p1->gametrace.back()->r, p1->gametrace.back()->c, WHITE);
 
 		// Show some progress to the screen.
-		cout << ".";
+        if (getenv("VERBOSE"))
+            cout << p2->toString() << endl;
+        else
+            cout << ".";
+
 		cout.flush();
    
 	}
@@ -81,14 +87,27 @@ int main(int argc, char **argv) {
 	cout << "p1->gameOutcome(WHITE) -> " << p1->gameOutcome(WHITE) << endl
 		 << "whtCaps -> " << p1->whtCaps << endl
 		 << "blkCaps -> " << p1->blkCaps << endl
+         << p2->toState().toString() << endl
 		 << p2->toString() << endl << endl;
 
+    if (getenv("DEBUG")) {
+        for (int i = 0; i < p2->Board.size(); i++) {
+            if (p2->getCell(i)->color == EMPTY)
+                continue;
+            
+            cout << "p->playMove(" << p2->getCell(i)->r << "," << 
+                p2->getCell(i)->c << ",";
+            
+            if (p2->getCell(i)->color == WHITE)
+                cout << "WHITE);" << endl;
+            else
+                cout << "BLACK);" << endl;
+        }
+    }
 
-	// Analyze the game and adjust weights accordingly.
 
-	/* Vtrain(b) <- V(Successor(b)) */
-	/* Adjust weights with wi <- wi + n(Vtrain(b) - V(b))*xi */
-	
+
+	// Analyze the game and train the neural network based on its error compared to Vhat()
 
 	PenteNeuralAI *games[] = {p1, p2};
 	int colors[] = {WHITE, BLACK};
@@ -111,50 +130,65 @@ int main(int argc, char **argv) {
 		State b = game->toState();
 		State successor = game->toState(); 
 
-		double error = 
-            game->gameOutcome(computer_color)*100 - weights.Vhat(b);
+        double weightsVhat = weights.Vhat(b);
+        
+        // Exit without computing error if weights is 0...
+        if (weightsVhat == 0)
+            continue;
 
-        vector<double> desired_output(1, weights.Vhat(b));
+		double error = 
+            game->toValue() - weightsVhat;
+
+        fann_type desired_output[1] = {weightsVhat};
 
         // Push in game->toState() as input to the neural net and 
         // use the vhat() as the expected output to train the network. 
 
         fann_train(ann,
                    game->neural_inputs(), /* The game inputs */
-                   (fann_type*)&desired_output[0]);
-
-		weights.adjust(b, error);
-
+                   desired_output /* the expected outputs */);
 
         cout << "Initial error: (" << 
-            game->gameOutcome(computer_color)*100 << " - " << 
-            weights.Vhat(b) << ") = " << error << endl;
+            game->toValue() << "(value) - " << 
+            weights.Vhat(b) << "(vhat(b) )) = " << error << endl;
 
         cout << "Errors => ";
 		while (game->gametrace.size()) {
-			successor = game->toState();
-		
 			// remember + remove the last move.
 			PenteNeuralAI::cell* tCell = game->gametrace.back();
 			game->gametrace.pop_back();
 			game->clearCell(tCell->r, tCell->c);
 
 			b = game->toState();
+            weightsVhat = weights.Vhat(b);
 
             if (getenv("VERBOSE") != NULL) {
-                error = weights.Vhat(b) - game->toValue();
-                cout << error << " ";
+                //error = weights.Vhat(b) - game->toValue();
+                cout << weightsVhat << ":" << game->toValue() << " ";
+                cout << b.toString() << endl;
             }
 
+            if (weightsVhat == 0) {
+                cout << ".";
+                cout.flush();
+                continue;
+            }
 
+            desired_output[0] = weightsVhat;
 
+            fann_train(ann,
+                       game->neural_inputs(),
+                       desired_output);
 
 		}
         cout << endl;
 	   
 	}
 
+
+    // Save the neural network back to disk.
     fann_save(ann, "pente.net");
+
     //fann_print_connections(ann);
     fann_destroy(ann);
 	return 0;
